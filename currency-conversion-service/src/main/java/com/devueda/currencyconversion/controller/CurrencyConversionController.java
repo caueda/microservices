57057732f962f3624a594898b9dc780e7ff4f1e9
Carlos R. Ueda <carlos.ueda@gmail.com>
2022-09-10 06:57:46 -0300

    Adding Eureka Server for service discovery

---
+++ currency-conversion-service/src/main/java/com/devueda/currencyconversion/controller/CurrencyConversionController.java
@@ -0,0 +1,58 @@
+package com.devueda.currencyconversion.controller;
+
+import com.devueda.currencyconversion.proxy.CurrencyExchangeProxy;
+import com.devueda.currencyconversion.model.CurrencyConversion;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+
+@RestController
+public class CurrencyConversionController {
+
+    private Environment environment;
+
+    private CurrencyExchangeProxy currencyExchangeProxy;
+
+    public CurrencyConversionController(Environment environment, CurrencyExchangeProxy currencyExchangeProxy) {
+        this.environment = environment;
+        this.currencyExchangeProxy = currencyExchangeProxy;
+    }
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversion> currencyConversion = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversion.class, uriVariables);
+        return CurrencyConversion.builder()
+                .id(100L)
+                .quantity(quantity)
+                .from(from)
+                .to(to)
+                .conversionMultiple(currencyConversion.getBody().getConversionMultiple())
+                .totalCalculatedAmount(quantity.multiply(currencyConversion.getBody().getConversionMultiple()))
+                .environment(environment.getProperty("local.server.port"))
+                .build();
+    }
+
+    @GetMapping("/currency-conversion-feign/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion calculateCurrencyConversionFeign(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+        CurrencyConversion currencyConversion = currencyExchangeProxy.retrieveExchangeValue(from, to);
+        return CurrencyConversion.builder()
+                .id(100L)
+                .quantity(quantity)
+                .from(from)
+                .to(to)
+                .conversionMultiple(currencyConversion.getConversionMultiple())
+                .totalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()))
+                .environment(environment.getProperty("local.server.port") + " feign")
+                .build();
+    }
+}
---
+++ naming-service/src/test/java/com/devueda/namingservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devueda.namingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
