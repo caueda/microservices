ec8da3b890794e7e5c36ed2af5b273236985c142
Carlos R. Ueda <carlos.ueda@gmail.com>
2022-09-11 07:57:00 -0300

    Examples of circuit breaker and RateLimit and Bulkhead

--- currency-exchange-service/src/main/java/com/devueda/currencyexchange/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/devueda/currencyexchange/controller/CircuitBreakerController.java
@@ -1,6 +1,8 @@
 package com.devueda.currencyexchange.controller;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +13,10 @@ import org.springframework.web.client.RestTemplate;
 @Slf4j
 public class CircuitBreakerController {
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+//    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+//    @RateLimiter(name="default")
+//    @Bulkhead(name="default")
     public String sampleApi() {
         log.info("Sample Api call received");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
