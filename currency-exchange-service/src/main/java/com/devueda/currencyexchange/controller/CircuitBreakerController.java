2aa40abb27559b4e3442d9e5009226d2895cf722
Carlos R. Ueda <carlos.ueda@gmail.com>
2022-09-12 06:54:34 -0300

    Zipkin

--- currency-exchange-service/src/main/java/com/devueda/currencyexchange/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/devueda/currencyexchange/controller/CircuitBreakerController.java
@@ -1,8 +1,6 @@
 package com.devueda.currencyexchange.controller;
 
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
--- currency-exchange-service/src/main/java/com/devueda/currencyexchange/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/devueda/currencyexchange/controller/CurrencyExchangeController.java
@@ -2,12 +2,14 @@ package com.devueda.currencyexchange.controller;
 
 import com.devueda.currencyexchange.model.CurrencyExchange;
 import com.devueda.currencyexchange.repository.CurrencyExchangeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class CurrencyExchangeController {
 
     private Environment environment;
@@ -20,6 +22,7 @@ public class CurrencyExchangeController {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        log.info("retrieveExchangeValue called with {} to {}", from, to);
         var currencyExchange = repository.findByFromAndTo(from, to);
         if(currencyExchange == null) {
             throw new RuntimeException(String.format("Unable to find data for %s and %s", from, to));
